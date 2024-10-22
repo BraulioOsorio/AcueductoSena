5d88c667831dde4cc9013ec2e29af8ce19e043d9
BraulioOsorio <braulioosoriomartinez@gmail.com>
2024-10-22 14:19:00 -0500

    server 2

--- pdfs/generarDocx.py
+++ pdfs/generarDocx.py
@@ -1,51 +1,47 @@
-from fastapi import HTTPException, Depends, Cookie, Response
-from fastapi import HTTPException
-import bcrypt
+from fastapi import FastAPI, Request, Depends, HTTPException, Cookie, Response, Form, status, Query
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from str_aleatorio import generar_random_id
-from fastapi import (
-    FastAPI,
-    Request,
-    Form,
-    status,
-    Depends,
-    HTTPException,
-    Cookie,
-    Query,
-)
-from models import Documento, Usuario, Empresa
 from fastapi.responses import JSONResponse, RedirectResponse
-
-from sqlalchemy.orm import Session, aliased
+from sqlalchemy.orm import Session
+from docx2pdf import convert  # Cambiar a docx2pdf
+from models import Documento, Usuario, Empresa
 from funciones import *
+from str_aleatorio import generar_random_id
+import bcrypt
+
 SUPER_ADMIN = "SuperAdmin"
 ADMIN = "Admin"
 datos_usuario = None
 
 app = FastAPI()
 
-# Agregando los archivos estaticos que están en la carpeta dist del proyecto
+# Agregando los archivos estáticos que están en la carpeta dist del proyecto
 app.mount("/static", StaticFiles(directory="public/dist"), name="static")
 
 template = Jinja2Templates(directory="public/templates")
 
 def manejarDocumentos(nombre_documento, datos, nit, id_usuario, db, id_servicio):
-    archivo = 'public/dist/ArchivosDescarga/'+nombre_documento+'.docx'
-    documento_modificado = reemplazar_texto(archivo, datos)
-    documento_modificado.save('public/dist/ArchivosDescarga/Generados/'+nombre_documento+'_Editado' + nit + '.docx')
+    archivo = 'public/dist/ArchivosDescarga/' + nombre_documento + '.docx'
     
-    archivo_docx = 'public/dist/ArchivosDescarga/Generados/'+nombre_documento+'_Editado' + nit + '.docx'
-    archivo_pdf = 'public/dist/ArchivosDescarga/Generados/'+nombre_documento+'' + nit + '.pdf'
+    # Modificar el documento
+    documento_modificado = reemplazar_texto(archivo, datos)
+    docx_path = f'public/dist/ArchivosDescarga/Generados/{nombre_documento}_Editado_{nit}.docx'
+    documento_modificado.save(docx_path)
 
-    convertir_a_pdf(archivo_docx, archivo_pdf)
+    # Convertir a PDF
+    archivo_pdf = f'public/dist/ArchivosDescarga/Generados/{nombre_documento}_{nit}.pdf'
+    try:
+        convert(docx_path, archivo_pdf)  # Convertir usando docx2pdf
+    except Exception as e:
+        print("Error al convertir a PDF:", e)
+        raise HTTPException(status_code=500, detail="Error al convertir el documento a PDF.")
 
-    nuevo_documento1 =   Documento(
+    nuevo_documento1 = Documento(
         id_usuario=id_usuario,
-        nom_doc= nombre_documento + nit,
+        nom_doc=f'{nombre_documento}_{nit}',
         id_servicio=id_servicio,
         tipo='pdf',
-        url='ArchivosDescarga/Generados/'+nombre_documento + nit + '.pdf'
+        url=f'ArchivosDescarga/Generados/{nombre_documento}_{nit}.pdf'
     )
 
     db.add(nuevo_documento1)
@@ -70,37 +66,26 @@ def generarDocx(
     diametro: str,
     caudal_permanente: str,
     rango_medicion: str
-    ):
-
+):
     if token:
-        is_token_valid = verificar_token(token, db)  # retorna el id_usuario
+        is_token_valid = verificar_token(token, db)
 
         if is_token_valid:
-
-            # Supongamos que tienes un ID de usuario específico
-
             rol_usuario = get_rol(is_token_valid, db)
             print(rol_usuario)
             headers = elimimar_cache()
             datos_usuario = get_datos_usuario(is_token_valid, db)
+
             if rol_usuario == ADMIN:
-
-                # Realizar una consulta para seleccionar los documentos asociados al usuario
+                # Eliminar documentos asociados al usuario
                 documentos_a_eliminar = db.query(Documento).filter(Documento.id_usuario == is_token_valid).all()
-
-                # Eliminar los documentos
                 for documento in documentos_a_eliminar:
                     db.delete(documento)
-
                 db.commit()    
                 id_usuario = is_token_valid
 
-            # Crea un alias para la tabla 'empresas' para la subconsulta
-                empresas_alias = aliased(Empresa)
-
-            # Realiza la consulta principal para obtener la información del usuario y su empresa relacionada
+                # Obtener información del usuario y su empresa
                 usuario = db.query(Usuario).filter(Usuario.id_usuario == id_usuario).first()
-
                 if usuario:
                     empresa = db.query(Empresa).filter(Empresa.id_empresa == usuario.empresa).first()
                     if empresa:
@@ -118,7 +103,7 @@ def generarDocx(
                             '[Campo Dirección]': empresa.direccion_empresa,
                             '[Campo Municipio]': municipio,
                             '[Campo Departamento]': departamento,
-                            '[Campo Teléfonos]': "Celular: "+empresa.tel_cel+" Telefono: :"+empresa.tel_fijo,
+                            '[Campo Teléfonos]': "Celular: " + empresa.tel_cel + " Telefono: " + empresa.tel_fijo,
                             '[Campo Página Web]': web,
                             '[Campo Correo]': empresa.email,
                             '[Campo Horario Atención]': horario,
@@ -133,24 +118,28 @@ def generarDocx(
                             '[Campo Caudal Permanente]': caudal_permanente,
                             '[Campo Rango Medicion]': rango_medicion,
                         }
+                        
                         arreglo_rutas = []
-                        #crear, guardar y convertir a pdf archivo 1
-                        manejarDocumentos("P01-F-03_Estatutos_Asociacion_Suscriptores", datos, nit, is_token_valid, db,1)
-                        arreglo_rutas.append('ArchivosDescarga/Generados/P01-F-03_Estatutos_Asociacion_Suscriptores'+ nit + '.pdf')
-                        manejarDocumentos("P01-F-02_Formato_Contrato_Condiciones_Uniformes", datos, nit, is_token_valid, db, 2)
-                        arreglo_rutas.append('ArchivosDescarga/Generados/P01-F-02_Formato_Contrato_Condiciones_Uniformes'+ nit + '.pdf')
+                        # Crear, guardar y convertir a PDF archivo 1
+                        manejarDocumentos("P01-F-03_Estatutos_Asociacion_Suscriptores", datos, nit, id_usuario, db, 1)
+                        arreglo_rutas.append(f'ArchivosDescarga/Generados/P01-F-03_Estatutos_Asociacion_Suscriptores_{nit}.pdf')
+                        manejarDocumentos("P01-F-02_Formato_Contrato_Condiciones_Uniformes", datos, nit, id_usuario, db, 2)
+                        arreglo_rutas.append(f'ArchivosDescarga/Generados/P01-F-02_Formato_Contrato_Condiciones_Uniformes_{nit}.pdf')
+
                         response = template.TemplateResponse(
-                        "paso-1/paso1-1/generar_documentos.html", {"request": request, "usuario": datos_usuario, "rutas_pdf": arreglo_rutas}
+                            "paso-1/paso1-1/generar_documentos.html", 
+                            {"request": request, "usuario": datos_usuario, "rutas_pdf": arreglo_rutas}
                         )
                         response.headers.update(headers)  # Actualiza las cabeceras
                         return response
-
                     else:
                         print('El usuario no está asociado a ninguna empresa.')
                 else:
                     print('No se encontró el usuario con el ID proporcionado.')
+
                 response = template.TemplateResponse(
-                    "paso-1/paso1-1/generar_documentos.html", {"request": request, "usuario": datos_usuario, "rutas_pdf": []}
+                    "paso-1/paso1-1/generar_documentos.html", 
+                    {"request": request, "usuario": datos_usuario, "rutas_pdf": []}
                 )
                 response.headers.update(headers)  # Actualiza las cabeceras
                 return response
@@ -160,7 +149,8 @@ def generarDocx(
                     "color": "warning",
                 }
                 response = template.TemplateResponse(
-                    "index.html", {"request": request, "alerta": alerta,"usuario": datos_usuario}
+                    "index.html", 
+                    {"request": request, "alerta": alerta, "usuario": datos_usuario}
                 )
                 response.headers.update(headers)  # Actualiza las cabeceras
                 return response
